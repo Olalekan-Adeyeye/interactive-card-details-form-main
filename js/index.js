4a02f874173de556817de27ad87e5f271002d552
Adeyeye Olalekan <adeyeye.lekan01@gmail.com>
2022-10-10 16:33:49 +0100

    Frontend Mentor Card Form Challenge V2.0

--- js/index.js
+++ js/index.js
@@ -2,65 +2,44 @@ const cardNumber = document.getElementById("card-number");
 const cardNumberHolder = document.querySelector(".card-number-space");
 const cardHolderName = document.getElementById("cardholder-name")
 const cardName = document.querySelector(".card-name")
-const cardNameError = document.querySelector(".cardholder-name-error")
 const cvc = document.querySelector(".cvc");
 const cvcInput = document.getElementById("cvc-input")
-const cvcError = document.querySelector(".cvc-error")
-const cardNumberError = document.querySelector(".card-number-error")
 const form = document.getElementById("form")
 const month = document.getElementById("exp-month")
 const year = document.getElementById("exp-year")
 const expMonthSpace = document.querySelector(".month")
 const expYearSpace = document.querySelector(".year")
-const expError = document.querySelector(".exp-error")
 const completedState = document.querySelector(".completed-state")
 const submit = document.getElementById("submit")
 const inputs = document.querySelectorAll("input")
-const error = document.querySelectorAll(".error")
 let isInvalid = []
 
-const checkForCardNumberError = () => {
-    if(isNaN(cardNumber.value) || cardNumber.value.includes(" ")){
-        cardNumberError.textContent = "Wrong format, numbers only"
-        console.log(cardNumberError.textContent)
-        cardNumberError.classList.remove("hidden")
-        isInvalid.push(false)
-    }
-    
-    if(!cardNumber.value){
-        cardNumberError.textContent = "Can't be blank"
-        isInvalid.push(false)
-    }
-
-}
-
-function checkForError(input, error, ){
-    if(!input.value) {       
-        error.classList.remove("hidden")
-        input.classList.add("error-border")
-        isInvalid.push(false)
-    }
-    else{
-        error.classList.add("hidden")
-        input.classList.remove("error-border")
-    }
-}
-
 function checkForEmptyValue(input, defaultValue,  spaceOnCard){
+    spaceOnCard.textContent = input.value
     if(!input.value){
         spaceOnCard.textContent = defaultValue
     }
 }
 
-const checkForNan = (input, error, nanError, blankError) => {
-    if(isNaN(input.value)){
-        error.textContent = nanError
+const checkForStringError = (input) =>{
+    if(!input.value || !isNaN(input.value)) {
+        input.classList.add("error-border")
+        isInvalid.push(false)
     }
     else{
-        error.textContent = blankError;
+        input.classList.remove("error-border")
     }
 }
 
+const checkForNanError = (input, requiredNumber) =>{
+    if(isNaN(input.value) || !input.value || input.value.length < requiredNumber){
+        input.classList.add("error-border")
+        isInvalid.push(false)
+    }
+    else{
+        input.classList.remove("error-border")
+    }
+}
 
 cardNumber.addEventListener('input', function(){
     if(cardNumber.value.length <= 16){
@@ -80,39 +59,34 @@ cardNumber.addEventListener('input', function(){
 })
 
 cardHolderName.addEventListener('input', function(){
-    cardName.textContent = cardHolderName.value
     checkForEmptyValue(cardHolderName, "Jane appleseed", cardName)
 })
 
 cvcInput.addEventListener("input", function(){
-    cvc.textContent = cvcInput.value
     checkForEmptyValue(cvcInput, "000", cvc)
-    
 })
 
 month.addEventListener("input", function(){
-    expMonthSpace.textContent = month.value
     checkForEmptyValue(month, "00", expMonthSpace)
 })
 
 year.addEventListener("input", function(){
-    expYearSpace.textContent = year.value
     checkForEmptyValue(year, "00", expYearSpace)
 })
 
+
 form.addEventListener("submit", function(e){
     e.preventDefault();
-    checkForError(cvcInput, cvcError) 
-    checkForError(cardNumber, cardNumberError)
-    checkForError(year, expError)
-    checkForError(month, expError)
-    checkForError(cardHolderName, cardNameError)
-    checkForCardNumberError()
-    if (!isInvalid.includes(false)) {
-        form.classList.add("invisible")
-        completedState.classList.remove("invisible")
-    }
-    isInvalid = []
+    checkForNanError(cardNumber, 15)
+    checkForNanError(cvcInput, 2)
+    checkForNanError(month, 1)
+    checkForNanError(year, 1)
+    checkForStringError(cardHolderName)
+        if (!isInvalid.includes(false)) {
+            form.classList.add("invisible")
+            completedState.classList.remove("invisible")
+        }
+        isInvalid = []
 })
 
 submit.addEventListener("click", ()=>{
